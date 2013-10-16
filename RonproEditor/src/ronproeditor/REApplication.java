e88b661e1b0a4be57702cf900cc13e5e07ad721c
Yoshiaki Matsuzawa <yoshiaki.matsuzzawa@gmail.com>
2013-10-16 09:12:10 +0900

    2.19.0 上記新バージョンを統合したweek3用バージョン．

--- RonproEditor/src/ronproeditor/REApplication.java
+++ RonproEditor/src/ronproeditor/REApplication.java
@@ -242,7 +242,8 @@ import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
  * 2013/09/26 version 2.18.0 ohata			・BE version 2.14.0と結合
  * 
  * 2013/10/11 version 2.18.1 hakamata		・DENO version0.2.4と統合
- * 											・DENOのBreakpoint, 実行位置表示モードの切り替え, cont, Focusのログ書き出し
+ * 											・DENOのBreakpoint, 実行位置表示モードの切り替え, cont, Focusのログ書き出し 
+ * 2013/10/16 version 2.19.0 matsuzawa		・上記新バージョンを統合したweek3用バージョン．	
  * 
  * ＜懸案事項＞
  * ・doCompile2()の設計が冗長なので再設計すること．
@@ -260,7 +261,7 @@ public class REApplication implements ICFwApplication {
 
 	// Application's Information.
 	public static final String APP_NAME = "Ronpro Editor";
-	public static final String VERSION = "2.18.1";
+	public static final String VERSION = "2.19.0";
 	public static final String BUILD_DATE = "2013/10/11";
 	public static final String DEVELOPERS = "Yoshiaki Matsuzawa & CreW Project & Sakai Lab";
 	public static final String COPYRIGHT = "Copyright(c) 2007-2012 Yoshiaki Matsuzawa & CreW Project & Sakai Lab. All Rights Reserved.";
