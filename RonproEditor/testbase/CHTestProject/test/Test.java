c6cc8b9256267cf557488212bb2accea526ed2d1
Yuya Kato <kato@sakailab.info>
2013-12-09 13:08:26 +0900

    gudsubのプロジェクト情報

---
+++ RonproEditor/testbase/CHTestProject/test/Test.java
@@ -0,0 +1,21 @@
+/**
+* プログラム名：
+* 作成者： 
+* 作成日： Tue Oct 22 00:17:33 JST 2013
+*/
+public class Test extends Turtle {
+	
+	//起動処理
+	public static void main(String[] args) {
+		Turtle.startTurtle(new Test());
+	}
+	
+	//タートルを動かす処理
+	public void start() {
+		for(int i=0; i<4; i++){
+			fd(50);
+			rt(90);
+		}
+	}
+	
+}
