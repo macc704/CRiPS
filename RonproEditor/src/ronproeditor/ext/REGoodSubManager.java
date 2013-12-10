d3af4239abae25a629158e2a7d6fff0dc2de36d1
Yuya Kato <kato@sakailab.info>
2013-12-10 23:54:31 +0900

    クライアント単体テストを可能にするための変更

--- RonproEditor/src/ronproeditor/ext/REGoodSubManager.java
+++ RonproEditor/src/ronproeditor/ext/REGoodSubManager.java
@@ -9,7 +9,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -32,8 +31,11 @@ public class REGoodSubManager {
 	private RESourceViewer viewer;
 	private JButton connButton;
 	private boolean started;
+	private SendObject sendObject = new SendObject();
+	CHMemberSelectorFrame frame;
 
 	public static void main(String[] args) {
+		new REGoodSubManager();
 	}
 
 	public REGoodSubManager(REApplication application) {
@@ -41,58 +43,22 @@ public class REGoodSubManager {
 		initialize();
 	}
 
+	public REGoodSubManager() {
+		connectServer();
+	}
+
 	private void initialize() {
 	}
 
 	public void startGoodSub() {
 
-		// initializeFrame();
 		initializeListener();
 
 		new Thread() {
 			public void run() {
-				// ê⁄ë±
-				try (Socket sock = new Socket("localhost", 10000)) {
-					conn = new Connection(sock);
-					newConnectionOpened(conn);
-				} catch (Exception ex) {
-					ex.printStackTrace();
-				}
+				connectServer();
 			}
 		}.start();
-
-	}
-
-	public void initializeFrame() {
-		// éÛêMëãê∂ê¨
-		// frame = new JFrame("CheCoPro");
-		// frame.setBounds(100, 100, 600, 500);
-		// frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		// sv = new RESourceViewer();
-		// btn = new JButton("stop");
-		// JSplitPane verticalSplitter = new
-		// JSplitPane(JSplitPane.VERTICAL_SPLIT);
-		//
-		// JSplitPane horizontalSplitter = new JSplitPane(
-		// JSplitPane.HORIZONTAL_SPLIT);
-		//
-		// List<JButton> btns = new ArrayList<JButton>();
-		// btns.add(new JButton("user1"));
-		// btns.add(new JButton("user2"));
-		// btnPanel = new JPanel();
-		// btnPanel.setLayout(new GridLayout(0, 1));
-		// JScrollPane scrollBtn = new JScrollPane();
-		//
-		// scrollBtn.setViewportView(btnPanel);
-		// horizontalSplitter.add(scrollBtn, JSplitPane.LEFT);
-		//
-		// verticalSplitter.setTopComponent(btn);
-		// verticalSplitter.setBottomComponent(sv);
-		//
-		// horizontalSplitter.add(verticalSplitter, JSplitPane.RIGHT);
-		//
-		// frame.getContentPane().add(horizontalSplitter, BorderLayout.CENTER);
-		// frame.setVisible(true);
 	}
 
 	public void initializeListener() {
@@ -104,32 +70,46 @@ public class REGoodSubManager {
 				conn.write(text);
 			}
 		});
-
-		// btn.addActionListener(new ActionListener() {
-		// @Override
-		// public void actionPerformed(ActionEvent e) {
-		// // TODO Auto-generated method stub
-		// String btnText = btn.getText();
-		// if (btnText == "start") {
-		// btn.setText("stop");
-		// } else if (btnText == "stop") {
-		// btn.setText("start");
-		// }
-		// }
-		// });
 	}
 
-	@SuppressWarnings("unchecked")
-	private void newConnectionOpened(Connection conn) {
+	private void connectServer() {
 
-		int groupNum = 0;
-		List<String> members = new ArrayList<String>();
-		SendObject sendObject = new SendObject();
+		try (Socket sock = new Socket("localhost", 10000)) {
+			conn = new Connection(sock);
+			newConnectionOpened(conn);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private void newConnectionOpened(Connection conn) {
 
 		conn.shakehandForClient();
 
-		// ïîâÆî‘çÜê›íË
+		if (login()) {
+			frame = new CHMemberSelectorFrame(sendObject.getMyName());
+			frame.open();
+			System.out.println("client established");
+			if (application != null) {
+				doOpenNewCHE();
+			}
+		}
+
+		try {
+			while (conn.established()) {
+				readFromServer();
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		conn.close();
+		System.out.println("client closed");
+
+	}
+
+	private boolean login() {
 		DivideRoom room = new DivideRoom();
+		int groupNum = -1;
 		try {
 			groupNum = room.selectRoomNum();
 		} catch (IOException e) {
@@ -141,43 +121,7 @@ public class REGoodSubManager {
 
 		conn.write(sendObject);
 
-		CHMemberSelectorFrame frame = new CHMemberSelectorFrame(
-				sendObject.getMyName());
-		frame.open();
-
-		if (conn.established()) {
-			System.out.println("client established");
-			doOpenNewCHE();
-		}
-
-		try {
-			while (conn.established()) {
-				Object obj = conn.read();
-				if (obj instanceof String) {
-					final String text = (String) obj;
-					SwingUtilities.invokeLater(new Runnable() {
-						public void run() {
-							if (isStarted()) {
-								chApplication.getFrame().getEditor()
-										.setText(text);
-							}
-						}
-					});
-				} else if (obj instanceof List) {
-					for (String aMember : (List<String>) obj) {
-						if (!members.contains(aMember)) {
-							members.add(aMember);
-						}
-					}
-					frame.setMembers(members);
-				}
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		conn.close();
-		System.out.println("client closed");
-
+		return conn.established();
 	}
 
 	public void doOpenNewCHE() {
@@ -207,19 +151,31 @@ public class REGoodSubManager {
 
 	}
 
+	@SuppressWarnings("unchecked")
+	private void readFromServer() {
+		List<String> members = new ArrayList<String>();
+		Object obj = conn.read();
+		if (obj instanceof String) {
+			final String text = (String) obj;
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					if (isStarted()) {
+						chApplication.getFrame().getEditor().setText(text);
+					}
+				}
+			});
+		} else if (obj instanceof List) {
+			for (String aMember : (List<String>) obj) {
+				if (!members.contains(aMember)) {
+					members.add(aMember);
+				}
+			}
+			frame.setMembers(members);
+		}
+	}
+
 	public boolean isStarted() {
 		return started;
-	}
-
-	public void fileSender() {
-		File file;
-		file = application.getSourceManager().getCurrentFile();
-		conn.write(file);
-	}
-
-	public void fileReceiver() {
-		application.doCreateProject();
-		application.doCreateFile();
 	}
 
 }
