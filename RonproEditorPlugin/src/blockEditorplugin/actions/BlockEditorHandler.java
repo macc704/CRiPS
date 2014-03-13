85ac5da96e3b91ff97fdd212a61a1644f4db3b76
Ohata <ohata@sakailab.info>
2014-03-13 17:33:48 +0900

    plugin.xmlの修正 ActinSetを拡張してBlockEditorの起動をしていたのを，Menu,Command,ect...を 拡張し起動するように変更

---
+++ RonproEditorPlugin/src/blockEditorplugin/actions/BlockEditorHandler.java
@@ -0,0 +1,22 @@
+package blockEditorplugin.actions;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+
+public class BlockEditorHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		new BlockEditorManager(window);
+		
+		return null;
+	}
+
+
+}
---
+++ RonproEditorPlugin/src/cocoviewer/actions/CocoviewerHandler.java
@@ -0,0 +1,18 @@
+package cocoviewer.actions;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+
+
+public class CocoviewerHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		//OpenCocoViewƒNƒŠƒbƒN‚Ìˆ—
+		return null;
+	}
+
+}
