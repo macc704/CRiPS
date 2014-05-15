578179edda05355c27257687d37016d2ebc1d465
Ohata <ohata@sakailab.info>
2014-05-15 11:19:08 +0900

    3.1.1 整数型引数のインクリメントがBlock化できなかった問題を修正 バージョン番号を変更：OOP対応をメジャーアップとして3.0.0とする

--- BlockEditor/src/a/slab/blockeditor/SBlockEditor.java
+++ BlockEditor/src/a/slab/blockeditor/SBlockEditor.java
@@ -281,12 +281,15 @@ import controller.WorkspaceController;
  *  	・スコープチェック　持っているブロック内の最初で変数宣言されている場合に正しく結合できない問題を修正
  *  2013.10.21 2.15.2 Ohata
  *  	・スコープチェック　基礎テスト終了版
- *  2014.04.08 2.16.0 Ohata
+ *  2014.04.08 3.0.0 Ohata
  *  	・OOP対応版
- *  2014.05.01 2.16.1　Ohata
+ *  2014.05.01 3.0.1　Ohata
  *  	・配列要素の参照ブロックの修正
- *  2014.05.02 2.16.1 Ohata
+ *  2014.05.02 3.0.1 Ohata
  *  	・配列要素への書き込みブロックの修正
+ *  2014.05.15 3.1.0 Ohata
+ *  	・引数ブロックの配列，obproライブラリ対応
+ *  
  *  
  * <TODO>
  * ・コメントにxmlのタグが入るとエラー．
@@ -307,7 +310,7 @@ public class SBlockEditor {
 
 	// frame name and version infomation
 	public final static String APP_NAME = "Block Editor";
-	public final static String VERSION = "2.16.2";
+	public final static String VERSION = "3.1.1";
 
 	public static final String ENCODING_BLOCK_XML = "UTF-8";
 	public static final boolean DEBUG = false;
