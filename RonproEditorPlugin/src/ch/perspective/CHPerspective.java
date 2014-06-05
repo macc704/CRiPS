e7335c9611b4686d2671c004f7cda08994d795cb
YuyaKato <kato@sakailab.info>
2014-06-05 18:02:03 +0900

    あげ忘れ

--- RonproEditorPlugin/src/ch/perspective/CHPerspective.java
+++ RonproEditorPlugin/src/ch/perspective/CHPerspective.java
@@ -11,25 +11,28 @@ public class CHPerspective implements IPerspectiveFactory{
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
 		// エディタ領域を取得
-        String editorArea = layout.getEditorArea();
+		String editorArea = layout.getEditorArea();
 
-        // ショートカット追加
-        layout.addPerspectiveShortcut("ch.perspective");
-        
-        // エディタ領域の左側にフォルダを追加
-        IFolderLayout left = layout.createFolder("left", IPageLayout.LEFT, 0.15f, editorArea);
-        left.addView(JavaUI.ID_PACKAGES);
-        
-        
-        // エディタ領域の下側にコンソールを表示
-       layout.addView("org.eclipse.ui.console.ConsoleView", IPageLayout.BOTTOM, 0.80f, editorArea);
-       
-       IFolderLayout editorRight = layout.createFolder("editorRight", IPageLayout.RIGHT, 0.50f, editorArea);
-       editorRight.addView(IPageLayout.ID_BOOKMARKS);
-       
-       // メンバエディタ領域の右側にフォルダを追加
-       IFolderLayout right = layout.createFolder("right", IPageLayout.RIGHT, 0.75f, "editorRight");
-       right.addView(IPageLayout.ID_TASK_LIST);
+		// ショートカット追加
+		layout.addPerspectiveShortcut("ch.perspective");
+
+		// エディタ領域の左側にフォルダを追加
+		IFolderLayout left = layout.createFolder("left", IPageLayout.LEFT, 0.15f, editorArea);
+		left.addView(JavaUI.ID_PACKAGES);
+
+
+		// エディタ領域の下側にコンソールを表示
+		IFolderLayout console = layout.createFolder("conlose", IPageLayout.BOTTOM, 0.80f, editorArea);
+		console.addView("org.eclipse.ui.console.ConsoleView");
+
+		IFolderLayout membersEditor = layout.createFolder("membersEditor", IPageLayout.RIGHT, 0.50f, editorArea);
+		membersEditor.addView(IPageLayout.ID_BOOKMARKS);
+
+		// メンバエディタ領域の右側にフォルダを追加
+		IFolderLayout memberProjects = layout.createFolder("memberProjects", IPageLayout.RIGHT, 0.75f, "membersEditor");
+		memberProjects.addView(IPageLayout.ID_TASK_LIST);
+
+		layout.addView(IPageLayout.ID_PROBLEM_VIEW, IPageLayout.RIGHT, 0.80f, "console");
 	}
 
 }
