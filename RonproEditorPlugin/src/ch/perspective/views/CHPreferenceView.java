86f6a999d0a658ff3b3760dd023e2e0f1349cf0f
YuyaKato <kato@sakailab.info>
2014-06-06 01:15:06 +0900

    CHPreferenceに，ユーザ名，パスワード，グループ番号入力欄追加

--- RonproEditorPlugin/src/ch/perspective/views/CHPreferenceView.java
+++ RonproEditorPlugin/src/ch/perspective/views/CHPreferenceView.java
@@ -1,20 +1,51 @@
 package ch.perspective.views;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
 
 public class CHPreferenceView extends ViewPart{
 
 	@Override
 	public void createPartControl(Composite parent) {
-		Label label = new Label(parent, SWT.NONE);
-		label.setText("HelloWorld");
+		
+		setComponents(parent);
 	}
 
 	@Override
 	public void setFocus() {
+	}
+	
+	public void setComponents(Composite parent){
+		
+		parent.setLayout(new GridLayout(2,true));
+		
+		// ユーザ名
+		Label userLabel = new Label(parent, SWT.NONE);
+		userLabel.setText("UserName");
+		Text userNameArea = new Text(parent, SWT.SINGLE | SWT.BORDER);
+		userNameArea.setTextLimit(16);
+		userNameArea.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		
+		// パスワード
+		Label passLabel = new Label(parent, SWT.NONE);
+		passLabel.setText("Password");
+		Text passArea = new Text(parent, SWT.PASSWORD | SWT.BORDER);
+		passArea.setTextLimit(16);
+		passArea.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		
+		// グループ番号
+		Label groupNumLabel = new Label(parent, SWT.NONE);
+		groupNumLabel.setText("GroupNumber");
+		Combo groupNumArea = new Combo(parent, SWT.READ_ONLY);
+		for(int i=0; i<51; i++){
+			groupNumArea.add(Integer.toString(i));
+		}
 	}
 
 }
