2dd8b604c60f2c89890a16bb046357f799c17a82
YuyaKato <kato@sakailab.info>
2014-06-05 17:11:31 +0900

    各ビューの領域の調整（ビューの中身未実装）

--- RonproEditorPlugin/src/ch/perspective/CHPerspective.java
+++ RonproEditorPlugin/src/ch/perspective/CHPerspective.java
@@ -1,5 +1,8 @@
 package ch.perspective;
 
+
+import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
@@ -10,13 +13,23 @@ public class CHPerspective implements IPerspectiveFactory{
 		// エディタ領域を取得
         String editorArea = layout.getEditorArea();
 
-        // エディタ領域の左側にリソース・ナビゲータビューを表示
-        layout.addView(IPageLayout.ID_RES_NAV
-                    , IPageLayout.LEFT, 0.25f, editorArea);
-
-        // エディタ領域の下側にブックマークビューを表示
-        layout.addView(IPageLayout.ID_BOOKMARKS
-                , IPageLayout.BOTTOM, 0.75f, editorArea);
+        // ショートカット追加
+        layout.addPerspectiveShortcut("ch.perspective");
+        
+        // エディタ領域の左側にフォルダを追加
+        IFolderLayout left = layout.createFolder("left", IPageLayout.LEFT, 0.15f, editorArea);
+        left.addView(JavaUI.ID_PACKAGES);
+        
+        
+        // エディタ領域の下側にコンソールを表示
+       layout.addView("org.eclipse.ui.console.ConsoleView", IPageLayout.BOTTOM, 0.80f, editorArea);
+       
+       IFolderLayout editorRight = layout.createFolder("editorRight", IPageLayout.RIGHT, 0.50f, editorArea);
+       editorRight.addView(IPageLayout.ID_BOOKMARKS);
+       
+       // メンバエディタ領域の右側にフォルダを追加
+       IFolderLayout right = layout.createFolder("right", IPageLayout.RIGHT, 0.75f, "editorRight");
+       right.addView(IPageLayout.ID_TASK_LIST);
 	}
 
 }
