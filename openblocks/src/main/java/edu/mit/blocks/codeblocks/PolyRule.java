e079f2011947669e5861e2194e2280e1dea3aefc
Ohata <ohata@sakailab.info>
2014-10-01 18:10:06 +0900

    poly型のソケットからブロックを取り外した際に，ソケットの形をpolyに戻すように変更

--- openblocks/src/main/java/edu/mit/blocks/codeblocks/PolyRule.java
+++ openblocks/src/main/java/edu/mit/blocks/codeblocks/PolyRule.java
@@ -1,8 +1,11 @@
 package edu.mit.blocks.codeblocks;
 
+import edu.mit.blocks.renderable.RenderableBlock;
 import edu.mit.blocks.workspace.WorkspaceEnvironment;
+import edu.mit.blocks.workspace.WorkspaceEvent;
+import edu.mit.blocks.workspace.WorkspaceListener;
 
-public class PolyRule implements LinkRule {
+public class PolyRule implements LinkRule,WorkspaceListener {
 
 	@Override
 	public boolean canLink(Block block1, Block block2, BlockConnector socket1,
@@ -26,6 +29,16 @@ public class PolyRule implements LinkRule {
 	public boolean isMandatory() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	@Override
+	public void workspaceEventOccurred(WorkspaceEvent event) {
+		//ohata added
+		// TODO Auto-generated method stub
+		if(event.getEventType() == WorkspaceEvent.BLOCKS_DISCONNECTED && "poly".equals(event.getSourceLink().getSocket().initKind())){
+			BlockLink link = event.getSourceLink();
+			link.getSocket().setKind("poly");
+		}
 	}
 
 	
