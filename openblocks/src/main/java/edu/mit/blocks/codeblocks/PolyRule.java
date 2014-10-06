2b0f444fcd8ce0874f0de226074d787d5a108aab
Ohata <ohata@sakailab.info>
2014-10-06 22:29:21 +0900

    polyルールを修正

--- openblocks/src/main/java/edu/mit/blocks/codeblocks/PolyRule.java
+++ openblocks/src/main/java/edu/mit/blocks/codeblocks/PolyRule.java
@@ -1,6 +1,5 @@
 package edu.mit.blocks.codeblocks;
 
-import edu.mit.blocks.renderable.RenderableBlock;
 import edu.mit.blocks.workspace.WorkspaceEnvironment;
 import edu.mit.blocks.workspace.WorkspaceEvent;
 import edu.mit.blocks.workspace.WorkspaceListener;
@@ -16,12 +15,7 @@ public class PolyRule implements LinkRule,WorkspaceListener {
 			return false;
 		}
 		
-		//ソケットの形を変更
-		if("poly".equals(socket1.getKind())){
-			socket1.setKind(socket2.getKind());
-		}else{
-			socket2.setKind(socket1.getKind());
-		}
+		
 		return true;
 	}
 
@@ -38,8 +32,32 @@ public class PolyRule implements LinkRule,WorkspaceListener {
 		if(event.getEventType() == WorkspaceEvent.BLOCKS_DISCONNECTED && "poly".equals(event.getSourceLink().getSocket().initKind())){
 			BlockLink link = event.getSourceLink();
 			link.getSocket().setKind("poly");
+			
+			Block block = event.getWorkspace().getEnv().getBlock(link.getSocketBlockID());
+			if(block.getGenusName().equals("callGetterMethod2")){
+				block.getPlug().setKind("poly");
+			}
+		}else if(event.getEventType() == WorkspaceEvent.BLOCKS_CONNECTED){
+			BlockLink link = event.getSourceLink();
+			//ソケットの形を変更
+			BlockConnector socket1 = link.getPlug();
+			BlockConnector socket2 = link.getSocket();
+			socket2.setKind("poly");
+			if("poly".equals(socket1.getKind())){
+				socket1.setKind(socket2.getKind());
+				
+				Block block = event.getWorkspace().getEnv().getBlock(socket2.getBlockID());
+				if(block.getGenusName().equals("callGetterMethod2")){
+					block.getPlug().setKind(socket2.getKind());
+				}
+			}else if("poly".equals(socket2.getKind())){
+				socket2.setKind(socket1.getKind());
+				
+				Block block = event.getWorkspace().getEnv().getBlock(socket1.getBlockID());
+				if(block.getGenusName().equals("callGetterMethod2")){
+					block.getPlug().setKind(socket1.getKind());
+				}
+			}
 		}
 	}
-
-	
 }
