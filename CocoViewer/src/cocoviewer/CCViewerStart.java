a3d5b00c2b0a14bd6c543c1913e7e9648ff17d2c
hirao0525 <hirao@sakailab.info>
2015-09-18 05:36:26 +0900

    CocoViewerリファクタリング：コンバータテストクラス，Viewerスタートテストクラスのコメントアウトをメソッドに変更

--- CocoViewer/src/cocoviewer/CCViewerStart.java
+++ CocoViewer/src/cocoviewer/CCViewerStart.java
@@ -1,5 +1,7 @@
 package cocoviewer;
 
+import clib.common.filesystem.CDirectory;
+import clib.common.filesystem.CFileSystem;
 import coco.controller.CCCompileErrorKindLoader;
 import coco.controller.CCCompileErrorLoader;
 import coco.controller.CCMetricsLoader;
@@ -16,24 +18,30 @@ public class CCViewerStart {
 		CCCompileErrorManager manager = new CCCompileErrorManager();
 		CCCompileErrorKindLoader kindloader = new CCCompileErrorKindLoader(
 				manager);
-		kindloader.load("ErrorKinds.csv");
+		kindloader.load("MyErrorKinds.csv");
 
 		CCCompileErrorLoader errorloader = new CCCompileErrorLoader(manager);
-		errorloader.load("CompileErrorLog.csv");
+		errorloader.load("CCCompileErrorLog.csv");
 
 		CCMetricsLoader metricsloader = new CCMetricsLoader(manager);
 		metricsloader.load("FileMetrics.csv");
-
-		// 事前にEclipseからPPVにかけておけば，ソースコードを閲覧できる（ただし時間がかかる）
-		// if (CFileSystem.getHomeDirectory().findDirectory(".ppvdata") != null)
-		// {
-		// CDirectory dir = CFileSystem.getHomeDirectory()
-		// .findOrCreateDirectory(".ppvdata");
-		// manager.setLibDir(dir.findOrCreateDirectory("ppv.lib"));
-		// manager.setBaseDir(dir);
-		// }
+		
+		// debug
+		// manager = setPPVdata(manager);
 
 		CCMainFrame2 frame = new CCMainFrame2(manager);
 		frame.setVisible(true);
+	}
+
+	// 事前にEclipseからPPVにかけておけば，ソースコードを閲覧できる（ただし時間がかかる）
+	private CCCompileErrorManager setPPVdata(CCCompileErrorManager manager) {
+		if (CFileSystem.getHomeDirectory().findDirectory(".ppvdata") != null)
+		{
+			CDirectory dir = CFileSystem.getHomeDirectory()
+					.findOrCreateDirectory(".ppvdata");
+			manager.setLibDir(dir.findOrCreateDirectory("ppv.lib"));
+			manager.setBaseDir(dir);
+		}
+		return manager;
 	}
 }
